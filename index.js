b9b233c03d3185b54b7fe2eaf91c67bdb4a6f423
zhfrann <muhzhafranilham13@gmail.com>
2023-06-19 17:03:00 +0800

    phonebook database exercise part3 3.18

--- index.js
+++ index.js
@@ -51,13 +51,15 @@ const generateId = () => {
     }
 }
 
-app.get('/info', (request, response) => {
-    Person.countDocuments().then(persons => {
-        response.send(`
-            <p>Phonebook has info for ${persons} people</p>
-            <p>${new Date()}</p>
-        `)
-    })
+app.get('/info', (request, response, next) => {
+    Person.countDocuments()
+        .then(persons => {
+            response.send(`
+                <p>Phonebook has info for ${persons} people</p>
+                <p>${new Date()}</p>
+            `)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
@@ -66,17 +68,28 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            if (result) {
+                response.status(204).end()
+            } else {
+                response.status(404).send({ error: "person not found" })
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -104,6 +117,39 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const newPerson = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = 3001;
 app.listen(PORT)
--- models/person.js
+++ models/person.js
@@ -4,6 +4,8 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
+console.log(`connecting to ${url}`);
+
 mongoose.connect(url)
     .then(result => {
         console.log("connected to MongoDB");
