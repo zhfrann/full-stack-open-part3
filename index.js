eb4db0e087113e94b3aa64e4e933b0771dbba8a9
zhfrann <muhzhafranilham13@gmail.com>
2023-06-20 12:42:38 +0800

    phonebook database backend exercise part3 3.21

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -113,20 +113,22 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    newPerson.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    newPerson.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
     const newPerson = {
-        name: body.name,
-        number: body.number
+        name: name,
+        number: number
     }
 
-    Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
+    Person.findByIdAndUpdate(request.params.id, newPerson, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -145,6 +147,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: "malformatted id" })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -15,8 +15,19 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: (props) => `invalid number ${props.value} format`
+        }
+    },
 });
 
 personSchema.set('toJSON', {
