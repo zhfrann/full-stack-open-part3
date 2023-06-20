1c1ba696b361e8bb0915e6150d27ff60c2811d1b
zhfrann <muhzhafranilham13@gmail.com>
2023-06-20 20:22:39 +0800

    update personSchema validation

--- models/person.js
+++ models/person.js
@@ -18,16 +18,19 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
-        minlength: 3
+        minlength: 3,
+        required: true
     },
     number: {
         type: String,
+        minlength: 8,
         validate: {
             validator: function (v) {
                 return /^\d{2,3}-\d+$/.test(v)
             },
             message: (props) => `invalid number ${props.value} format`
-        }
+        },
+        required: true
     },
 })
 
